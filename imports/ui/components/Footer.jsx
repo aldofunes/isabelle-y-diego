5a123ce2744b3400b4d626bcf02ce964a9c518dc
Aldo Funes <aldofunes88@gmail.com>
2016-05-22 07:52:12 -0500

    Place thanks to BMG Tech

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -5,8 +5,12 @@ export default class Footer extends React.Component {
     return (
       <section id="footer">
         <div className="ui center aligned container">
+          <h3 className="ui header">Nuestros más sinceros agradecimientos</h3>
           <p>
-            Designed and developed by <a href="https://www.bmgtech.io" target="_blank">BMG Tech</a>
+            Este sitio web es cortesía de la familia Funes Minutti a través de
+            <a href="https://www.bmgtech.io" target="_blank"> BMG Tech</a>. Contacto:
+            <a href="mailto:aldo.funes@bmgtech.io"> Aldo Funes</a> y
+            <a href="mailto:pamela.viadana@bmgtech.io"> Pamela Viadana</a>.
           </p>
         </div>
       </section>
