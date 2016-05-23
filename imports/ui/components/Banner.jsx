c981e368fe21380fd7e3a84027ca3e677a084491
Aldo Funes <aldofunes88@gmail.com>
2016-05-23 08:35:27 -0500

    574290a7bce4f52d7a484d58 Cambiar tipografía de "Hoy vamos juntos hasta la cima…"

--- imports/ui/components/Banner.jsx
+++ imports/ui/components/Banner.jsx
@@ -5,11 +5,11 @@ export default class Banner extends Component {
     return (
       <section id="banner">
         <div className="text-box center-block">
-          <h2 className="ui header">Isabelle y Diego</h2>
+          <h1 className="ui header">Isabelle y Diego</h1>
           <p>13 de agosto, 2016</p>
-          <h4 className="ui header">
+          <h2 className="ui header headline">
             Hoy vamos juntos hasta la cima, esto es un sueño hecho realidad
-          </h4>
+          </h2>
         </div>
       </section>
     );
