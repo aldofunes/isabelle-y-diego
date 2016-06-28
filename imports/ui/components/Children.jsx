f298c988f409d2377a5e1de1cb88303ab543374d
Aldo Funes <aldofunes88@gmail.com>
2016-06-27 20:03:52 -0500

    Translate and improve performance

--- imports/ui/components/Children.jsx
+++ imports/ui/components/Children.jsx
@@ -5,34 +5,38 @@ const markdown = require('markdown-it')({
   breaks: true, // Convert '\n' in paragraphs into <br>
 });
 
-const children = [
-  {
-    _id: '1',
-    name: 'Diego',
-    bio: i18n.__('home.children.diego'),
-    picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/diego-junior.jpg',
-  },
-  {
-    _id: '2',
-    name: 'Isabelle',
-    bio: i18n.__('home.children.isabelle'),
-    picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/isabelle-hija.jpg',
-  },
-  {
-    _id: '3',
-    name: 'Anne Hélène',
-    bio: i18n.__('home.children.anneHelene'),
-    picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/anne-helene.jpg',
-  },
-  {
-    _id: '4',
-    name: 'Catalina',
-    bio: i18n.__('home.children.catalina'),
-    picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/catalina.jpg',
-  },
-];
-
 export default class Children extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.children = [
+      {
+        _id: '1',
+        name: 'Diego',
+        bio: i18n.__('home.children.diego'),
+        picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/diego-junior.jpg',
+      },
+      {
+        _id: '2',
+        name: 'Isabelle',
+        bio: i18n.__('home.children.isabelle'),
+        picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/isabelle-hija.jpg',
+      },
+      {
+        _id: '3',
+        name: 'Anne Hélène',
+        bio: i18n.__('home.children.anneHelene'),
+        picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/anne-helene.jpg',
+      },
+      {
+        _id: '4',
+        name: 'Catalina',
+        bio: i18n.__('home.children.catalina'),
+        picture: 'https://d1w4wvwm5jykke.cloudfront.net/IsabelleYDiego/family/catalina.jpg',
+      },
+    ];
+  }
+  
   rawMarkup(bio) {
     return { __html: markdown.render(bio) };
   }
@@ -43,7 +47,7 @@ export default class Children extends React.Component {
         <div className="ui container center aligned">
           <h2>{i18n.__('home.children.header')}</h2>
           <div className="ui middle aligned four column doubling grid">
-            {children.map((child) => (
+            {this.children.map((child) => (
               <div key={child._id} className="column">
                 <img className="ui centered small circular image" src={child.picture} />
                 <h3 className="header">{child.name}</h3>
